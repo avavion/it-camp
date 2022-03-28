0c5ea3c21d8e509d2522eca2f02343c20d6e2584
Alan Bogov (avavion) <avavionmvm@gmail.com>
2022-03-28 20:47:04 +0300

    init

---
+++ js/index.js
@@ -0,0 +1,56 @@
+const calculator = () => {
+    const element = document.querySelector('.calculator');
+
+    if (!element) return false;
+
+    const total_price = document.getElementById('total_price');
+    const selects = element.querySelectorAll('select');
+    const checkboxes = element.querySelectorAll('input[type="checkbox"]');
+
+    const calculate = () => {
+        let total = 0;
+
+        for (const select of selects) {
+            total += parseInt(select.options[select.selectedIndex].getAttribute('data-price'));
+        }
+
+        for (const checkbox of checkboxes) {
+            if (checkbox.checked) {
+                total += parseInt(checkbox.getAttribute('data-price'));
+            }
+        }
+
+        total_price.textContent = total;
+    }
+
+    for (const select of selects) {
+        select.addEventListener('change', calculate);
+    }
+
+    for (const checkbox of checkboxes) {
+        checkbox.addEventListener('change', calculate);
+    }
+
+    calculate();
+}
+
+const slider = () => {
+    
+    const container = document.querySelector(".swiper");
+
+    const option = {
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        }
+    }
+
+    const swiper = new Swiper(container, option)
+}
+
+const init = () => {
+    calculator();
+    slider();
+}
+
+document.addEventListener('DOMContentLoaded', init);
